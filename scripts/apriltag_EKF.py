2960c927e6873dbf653322316b4ebedf437ae8f2
Benjamin Wong <bycw@uw.edu>
2024-01-11 23:34:49 -0800

    test

--- scripts/apriltag_EKF.py
+++ scripts/apriltag_EKF.py
@@ -324,7 +324,7 @@ class EKF:
             
             H=np.zeros((6,7)) #number of obervation: 6, number of state:7 
             H[0:3, 0:6] = J_loc
-         #   H[3:6, 2] = jr
+            H[3:6, 2] = jr
             H[3:6:, 6] = jtag
             
             F=np.zeros((7,mu.shape[0]))
