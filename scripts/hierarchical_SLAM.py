c070ceefcd1af88ba5bbda9d51b7c5b9865cb6d7
Benjamin Wong <bycw@uw.edu>
2024-03-07 21:45:48 -0800

    fix bug

--- scripts/hierarchical_SLAM.py
+++ scripts/hierarchical_SLAM.py
@@ -329,6 +329,7 @@ class Graph_SLAM:
         self.init_new_features(mu, node_to_origin, features)
         delta=norm(mu[0:3])
         if delta>=1.5:
+            print(sigma)
             self._posterior_to_factor(mu, sigma)
             _, H=self.back_end.optimize(self.front_end)
             self.omega=H
