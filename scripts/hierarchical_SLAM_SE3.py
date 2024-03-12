17b9c58fc0d239a1b8318ebb6d4adfa4125f8322
Benjamin Wong <bycw@uw.edu>
2024-03-12 16:11:50 -0700

    fixed publisher

--- scripts/hierarchical_SLAM_SE3.py
+++ scripts/hierarchical_SLAM_SE3.py
@@ -419,7 +419,7 @@ def get_landmark_markers(nodes):
     
         marker = Marker()
         marker.type = 0
-        marker.id = "f" + str(node.id)
+        marker.id = 200+ node.id
         
         marker.header.frame_id = "map"
         marker.header.stamp = rospy.Time.now()
