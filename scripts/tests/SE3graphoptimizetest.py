fixed jacobian

scripts/tests/SE3graphoptimizetest.py
@@ -86,45 +86,36 @@ class Graph_SLAM:
             self.factors.append(self.Factor(parent,child,features ,z,sigma, idx_map))
                 
         
-class Back_end:
-    # def get_pose_jacobian(self, tau_1, tau_2, z_bar):
-    #     J1=-SE3.Jl_inv(z_bar)
-    #     J2=SE3.Jr_inv(z_bar)
-    #     return J1, J2
-    
-  
-    # def get_feature_jacobian(self, tau_1, tau_2, z_bar):
-    #     J1=-SE3.Jl_inv(z_bar)@SE3.Jr(tau_1)
-    #     J2=SE3.Jr_inv(z_bar)@SE3.Jr(tau_2)
-    #     return J1, J2
-    
+class Back_end:    
     def __init__(self):
         pass
     
     def node_to_vector(self, graph):
         self.pose_idx_map={}
         self.feature_idx_map={}
-        mu=[]
+        n = 0 
         for node_id, node in graph.pose_nodes.items():
             if not node.pruned:
-                self.pose_idx_map[node_id]=len(mu)*6
-                mu.append(node.M)
+                self.pose_idx_map[node_id]=n*6
+                n+=1
+                
         for node_id,node in graph.feature_nodes.items():
-            self.feature_idx_map[node_id]=len(mu)*6
-            mu.append(node.M)
-        return mu
+            self.feature_idx_map[node_id]=n*6
+            n+=1
+
+        return n
     
-    def linearize(self,x, factors):
-        H = np.zeros((6*len(x), 6*len(x)))
-        b = np.zeros(6*len(x))
+    def linearize(self,n, factors):
+        H = np.zeros((6*n, 6*n))
+        b = np.zeros(6*n)
         for factor in factors:
             idx_map = factor.idx_map.copy()
             omega = factor.omega.copy()
-            # omega = 0.1*np.eye(len(factor.omega))
+            # omega = np.eye(len(factor.omega))
             if not factor.parent == None:
-                F = np.zeros((6*len(x), 12+factor.n*6))          #map from factor vector to graph vector
+                F = np.zeros((6*n, 12+factor.n*6))          #map from factor vector to graph vector
                 J = np.zeros((6+factor.n*6,12+factor.n*6)) #map from factor vector to observation
-                e = np.zeros((6+factor.n*6)) #difference between observation and expected observation
+                e = np.zeros(len(factor.z)) #difference between observation and expected observation
                  
                 idx=self.pose_idx_map[factor.parent.id]
                 F[idx:idx+6,0:6] = np.eye(6)
@@ -133,10 +124,8 @@ class Back_end:
                 M_r1_inv = inv(M_r1)
                 M_r2 = factor.child.M.copy()
                 z_bar = SE3.Log(M_r1_inv@M_r2)
-                J1=-SE3.Jl_inv(z_bar)
-                J2=SE3.Jr_inv(z_bar)
-                J[0:6,0:6] = J1
-                J[0:6, 6:12] = J2
+                J[0:6,0:6] = -SE3.Jl_inv(z_bar)
+                J[0:6, 6:12] = SE3.Jr_inv(z_bar)
                 e[0:6] = z - z_bar
                 
                 idx=self.pose_idx_map[factor.child.id]
@@ -147,17 +136,15 @@ class Back_end:
                     z = factor.z[i:i+6].copy()
                     z_bar = SE3.Log(M_r1_inv@feature.M.copy())
 
-                    J1=-SE3.Jl_inv(z_bar)
-                    J2=SE3.Jr_inv(z_bar)
-                    J[i:i+6, 0:6] = J1
-                    J[i:i+6, 6+i:6+i+6] = J2
-                    e[i:i+6] = z - z_bar
-                    
+                    J[i:i+6, 0:6] = -SE3.Jl_inv(z_bar)
+                    J[i:i+6, 6+i:6+i+6] = SE3.Jr_inv(z_bar)
+                    if not feature.id == 12:
+                        e[i:i+6] = z - z_bar
                     idx=self.feature_idx_map[feature.id]
                     F[idx:idx+6,6+i:6+i+6] = np.eye(6)
             else:
                 J = np.eye(len(factor.z))
-                F = np.zeros((6*len(x), len(factor.z)))   
+                F = np.zeros((6*n, len(factor.z)))   
                 e = np.zeros(len(factor.z))
                 if not factor.child == None:
                     z = factor.z[i:i+6].copy()
@@ -175,61 +162,59 @@ class Back_end:
                     e[i:i+6] = z - z_bar
                     idx=self.feature_idx_map[feature.id]
                     F[idx:idx+6,i:i+6] = np.eye(6)
-                #print(e)
-                global test
-                test=F@J.T@omega@J@F.T
-            H+=F@J.T@omega@J@F.T
+            global test
+            test=F@(J.T@omega@J)@F.T
+            print(e)
+
+            H+=F@(J.T@omega@J)@F.T
             b+=F@J.T@omega@e
 
         return H, b
     
     def linear_solve(self, A,b):
-        A=(A+A.T)/2
+        # A=(A+A.T)/2
         # L=np.linalg.cholesky(A)
         # y=solve_triangular(L,b, lower=True)
         
-        #return solve_triangular(L.T, y)
+        # return solve_triangular(L.T, y)
         return lstsq(A,b)[0]
     
     def update_nodes(self, graph,dx, cov):
-        for node in graph.pose_nodes.values():
-            if not node.pruned:
-                idx=self.pose_idx_map[node.id]
-                node.M=node.M@SE3.Exp(dx[idx:idx+6])
-                node.H=cov[idx:idx+6,idx:idx+6].copy()
-                
-        for node in graph.feature_nodes.values():
-            idx=self.feature_idx_map[node.id]
-            node.M=node.M@SE3.Exp(dx[idx:idx+6])
-            node.H=cov[idx:idx+6,idx:idx+6].copy()
+        for node_id, idx in self.pose_idx_map.items():
+            graph.pose_nodes[node_id].M = graph.pose_nodes[node_id].M@SE3.Exp(dx[idx:idx+6])
+            graph.pose_nodes[node_id].H = cov[idx:idx+6,idx:idx+6].copy()
+  
+        for node_id, idx in self.feature_idx_map.items():
+            graph.feature_nodes[node_id].M = graph.feature_nodes[node_id].M@SE3.Exp(dx[idx:idx+6])
+            graph.feature_nodes[node_id].H = cov[idx:idx+6,idx:idx+6].copy()
 
         
     def optimize(self, graph):
-        with open('graph.pickle', 'wb') as handle:
-            pickle.dump(graph, handle)
+        # with open('graph.pickle', 'wb') as handle:
+        #     pickle.dump(graph, handle)
         print("optimizing graph")
-        x = self.node_to_vector(graph)
-        H,b=self.linearize(x,graph.factors)
+        n = self.node_to_vector(graph)
+        H,b=self.linearize(n,graph.factors.copy())
         dx=self.linear_solve(H,b)
-        # x+=dx 
+        # global dx_test
+        # dx_test=dx
         i=0
-        self.update_nodes(graph, 0.01*dx.copy(),np.zeros(H.shape))
-        while np.max(np.abs(dx))>0.01 and i<10000:
+        self.update_nodes(graph, 1*dx.copy(),np.zeros(H.shape))
+        while np.max(np.abs(dx))>0.01 and i<1000:
             print(i)
-            H,b=self.linearize(x,graph.factors)
+            H,b=self.linearize(n,graph.factors)
             global dx_test
             dx_test=dx
             print(np.max(np.abs(dx)))
             dx=self.linear_solve(H,b)
-            # x+= dx
-            self.update_nodes(graph, 0.01*dx.copy(),np.zeros(H.shape))
+            self.update_nodes(graph, 1*dx.copy(),np.zeros(H.shape))
             i+=1
 
 
-        self.update_nodes(graph, np.zeros(6*len(x)),inv(H))
+        self.update_nodes(graph, np.zeros(len(dx)),inv(H))
         print("optimized")
 
-        return x, H
+        return H
     
 solver=Back_end()
 with open('graphSE3.pickle', 'rb') as handle:
@@ -242,22 +227,30 @@ for factor in graph.factors:
         M2 = factor.child.M
         centroid = [(M1[0,3]+M2[0,3])/2, (M1[1,3]+M2[1,3])/2]
         plt.plot((M1[0,3], M2[0,3]), (M1[1,3], M2[1,3]), "--", color=(0.5,0.5,0.5))
-        plt.plot(centroid[0], centroid[1] , "s", color='k')
+       # plt.plot(centroid[0], centroid[1] , "s", color='k')
+        M2 = M1@SE3.Exp(factor.z[0:6])
+        plt.plot(M2[0,3], M2[1,3], "o", color=(0.5,0.5,0.5, 0.5), markersize = 20)
+
         for feature in factor.feature_nodes:
+            i=factor.idx_map[feature.id]
+            M2 = M1@SE3.Exp(factor.z[i:i+6])
+            plt.plot(M2[0,3], M2[1,3], "o", color=(0.5,0.5,0.5, 0.5), markersize = 20)
+            plt.plot((M1[0,3], M2[0,3]), (M1[1,3], M2[1,3]), "--", color=(0.5,0.5,0.5))
+            plt.text(M2[0,3], M2[1,3], feature.id)
+
             M2 = feature.M
             plt.plot((centroid[0], M2[0,3]), (centroid[1], M2[1,3]), "--", color=(0.5,0.5,0.5))
             
 for node in graph.pose_nodes.values():
     M=node.M
     mu=SE3.Log(M)
-    print(M)
     plt.plot(M[0,3], M[1,3], "o", color="k")
     plt.arrow(M[0,3], M[1,3], 0.5*cos(mu[5]), 0.5*sin(mu[5]))
     
 for node in graph.feature_nodes.values():
     M=node.M
     mu=SE3.Log(M)
-    print(M)
+    plt.text(M[0,3], M[1,3], node.id)
 
     plt.plot(M[0,3], M[1,3], "*", markersize=20)
     plt.arrow(M[0,3], M[1,3], 0.5*cos(mu[5]), 0.5*sin(mu[5]))
@@ -265,42 +258,53 @@ for node in graph.feature_nodes.values():
 
 
 plt.axis('scaled')
-    
-x, H = solver.optimize(graph)
+plt.xlim([-6, 1])
+plt.ylim([-1, 5])
+plt.title("init")
+#%%
+H = solver.optimize(graph)
 #%%
 plt.figure(1)
 for factor in graph.factors:
-    omega=factor.omega
-    print("min",np.min(np.abs(omega)))
-    print(np.min(np.linalg.eig(omega)[0]))
     if not factor.parent == None:
         M1 = factor.parent.M
         M2 = factor.child.M
         centroid = [(M1[0,3]+M2[0,3])/2, (M1[1,3]+M2[1,3])/2]
         plt.plot((M1[0,3], M2[0,3]), (M1[1,3], M2[1,3]), "--", color=(0.5,0.5,0.5))
-        plt.plot(centroid[0], centroid[1] , "s", color='k')
+       # plt.plot(centroid[0], centroid[1] , "s", color='k')
+        M2 = M1@SE3.Exp(factor.z[0:6])
+        plt.plot(M2[0,3], M2[1,3], "o", color=(0.5,0.5,0.5, 0.5), markersize = 20)
+
         for feature in factor.feature_nodes:
+            i=factor.idx_map[feature.id]
+            M2 = M1@SE3.Exp(factor.z[i:i+6])
+            plt.plot(M2[0,3], M2[1,3], "o", color=(0.5,0.5,0.5, 0.5), markersize = 20)
+            plt.plot((M1[0,3], M2[0,3]), (M1[1,3], M2[1,3]), "--", color=(0.5,0.5,0.5))
+
             M2 = feature.M
             plt.plot((centroid[0], M2[0,3]), (centroid[1], M2[1,3]), "--", color=(0.5,0.5,0.5))
             
 for node in graph.pose_nodes.values():
     M=node.M
     mu=SE3.Log(M)
-    print(M)
+    # print(M)
     plt.plot(M[0,3], M[1,3], "o", color="k")
     plt.arrow(M[0,3], M[1,3], 0.5*cos(mu[5]), 0.5*sin(mu[5]))
     
 for node in graph.feature_nodes.values():
     M=node.M
     mu=SE3.Log(M)
-    print(M)
-
+    # print(M)
+    plt.text(M[0,3], M[1,3], node.id)
     plt.plot(M[0,3], M[1,3], "*", markersize=20)
     plt.arrow(M[0,3], M[1,3], 0.5*cos(mu[5]), 0.5*sin(mu[5]))
     
 
 
 plt.axis('scaled')
+plt.xlim([-6, 1])
+plt.ylim([-1, 5])
+plt.title("optmized")
 
 #%%
 graph.feature_nodes[12].M@inv(SE3.Exp(graph.factors[1].z[6:12]))