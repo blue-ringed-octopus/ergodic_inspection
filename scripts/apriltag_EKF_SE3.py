2b900727e06ef7ee3d3754145447a44ab16844a7
Benjamin Wong <bycw@uw.edu>
2024-04-14 17:35:58 -0700

    test cov

--- scripts/apriltag_EKF_SE3.py
+++ scripts/apriltag_EKF_SE3.py
@@ -182,8 +182,8 @@ class EKF:
         self.Q[3:6, 3:6] *= (np.pi/2)**2 #axis angle
         
         self.Q_img = np.eye(3)
-        self.Q_img[0,0]=1**2 #x-pixel
-        self.Q_img[1,1]=1**2 #y-pixel
+        self.Q_img[0,0]=2**2 #x-pixel
+        self.Q_img[1,1]=2**2 #y-pixel
         self.Q_img[2,2]=0.01**2 #depth 
         
         self.at_detector = Detector(
