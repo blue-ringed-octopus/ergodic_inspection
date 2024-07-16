f24fb69abc8adc91205477446f6208c045a94412
Benjamin Wong <bycw@uw.edu>
2024-07-15 17:53:03 -0700

    test graph server

--- scripts/ergodic_planner_server.py
+++ scripts/ergodic_planner_server.py
@@ -7,7 +7,12 @@ Created on Mon Jul  8 17:49:39 2024
 
 from ergodic_planner import Ergodic_Planner
 import rospy
+from ergodic_inspection.srv import GetGraphStructure
+
 
 if __name__ == '__main__':
-    rospy.wait_for_service('set_entropy')
-    region_planner = Ergodic_Planner(map_manager.hierarchical_graph.levels[1])
+    rospy.wait_for_service('GetGraphStructure')
+    get_graph = rospy.ServiceProxy('GetGraphStructure', GetGraphStructure)
+    msg = get_graph(1)
+    print(msg)
+    # region_planner = Ergodic_Planner(map_manager.hierarchical_graph.levels[1])
