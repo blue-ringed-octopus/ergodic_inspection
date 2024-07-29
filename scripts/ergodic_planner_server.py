101bd83c61158b83c594cb726ea63a58d1b2e05a
Benjamin Wong <bycw@uw.edu>
2024-07-29 13:26:13 -0700

    fix index

--- scripts/ergodic_planner_server.py
+++ scripts/ergodic_planner_server.py
@@ -26,8 +26,8 @@ class Ergodic_Planner_Server:
     def plan_region_handler(self, req):
         msg = self.get_graph(1)
         _, _, _, w = self.parse_graph_msg(msg)
-        region = self.id_map[req.current_region]
-        next_region = self.planner.get_next_region(w, region)
+        region = self.id_map[int(req.current_region)]
+        next_region, _ = self.planner.get_next_region(w, region)
         return PlanRegionResponse(str(next_region))
     
     def parse_graph_msg(self, msg):
