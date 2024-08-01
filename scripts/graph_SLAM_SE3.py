24762d4f5a625d7aa7c9370b6b68297cf3b9694b
Benjamin Wong <bycw@uw.edu>
2024-08-01 16:46:42 -0700

    param yaml

--- scripts/graph_SLAM_SE3.py
+++ scripts/graph_SLAM_SE3.py
@@ -495,18 +495,22 @@ class Graph_SLAM:
                 self.factor_graph.feature_nodes[node_id].M = M[idx]
                 self.factor_graph.feature_nodes[node_id].cov = cov[6*idx:6*idx+6,6*idx:6*idx+6].copy()
         print("update end")
+        
     def optimize(self):
         print("optimizing")
         # with open('graph.pickle', 'wb') as handle:
         #     pickle.dump(self.factor_graph, handle)
         M, H, idx_map = self.back_end.optimize(deepcopy(self.factor_graph), self.localize_mode)
+        print("apply results")
         self.update_nodes(M, H, idx_map)
 
         self.omega = H
         # self.global_map_assemble()
         self.optimized = True
+        print("start prune")
         self.factor_graph.prune(10, self.localize_mode)
         print("optimize end")
+        
 if __name__ == "__main__":
     graph_slam = Graph_SLAM(np.zeros(4), True, 1, 0, 1000)
     with open('tests/graph.pickle', 'rb') as f:
