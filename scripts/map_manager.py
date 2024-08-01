77d4424eb1e47848f0879f9c63ca23acabe2c529
Benjamin Wong <bycw@uw.edu>
2024-07-31 20:51:14 -0700

    test graph slam

--- scripts/map_manager.py
+++ scripts/map_manager.py
@@ -130,7 +130,7 @@ class Map_Manager:
         entropy=np.zeros(len(self.region_idx))
         for i, idx in enumerate(self.region_idx):
             h = self.h[idx]
-            entropy[i] = np.quantile(h,0.5)
+            entropy[i] = np.quantile(h,0.75)
             # entropy[i] = np.mean(h)
         return entropy 
     
