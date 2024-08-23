a0daa47872d0a4b986404f7741c2cc368bb5508a
Benjamin Wong <bycw@uw.edu>
2024-08-23 14:24:31 -0700

    ekf point cloud

--- scripts/apriltag_EKF_SE3.py
+++ scripts/apriltag_EKF_SE3.py
@@ -156,8 +156,12 @@ class EKF:
         self.sigma=np.zeros((6,6))
         self.features={}
         self.landmarks=landmarks
+        self.cloud = {"pc": [],"cov": [], "depth": [], "rgb": [], "features": {} ,"cam_param": self.K.copy(), "cam_transform": self.T_c_to_r.copy()}
+
         if not pc_info == None:
             self._process_pointcloud(pc_info)
+        
+            
         # self._initialize_landmarks(landmark)
     
     # def _initialize_landmarks(self, landmarks):
