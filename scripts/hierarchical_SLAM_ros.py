a9b375aae28ad38f169ee96c63091c71d8a6314a
Benjamin Wong <bycw@uw.edu>
2024-10-03 14:45:29 -0700

    seperate sim params

--- scripts/hierarchical_SLAM_ros.py
+++ scripts/hierarchical_SLAM_ros.py
@@ -23,6 +23,7 @@ from ergodic_inspection.srv import PlaceNode, PlaceNodeResponse, OptimizePoseGra
 
 np.float = np.float64 
 np.set_printoptions(precision=2)
+is_sim = rospy.get_param("isSim")
 
     
 class Graph_SLAM_wrapper:
@@ -312,7 +313,6 @@ if __name__ == "__main__":
     import pickle
     rospack=rospkg.RosPack()
     path = rospack.get_path("ergodic_inspection")
-    is_sim = rospy.get_param("isSim")
     
     if is_sim:
         param_path = path + "/param/sim/"
