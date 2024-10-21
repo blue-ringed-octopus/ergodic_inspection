237fa71d2c0d817af6bdab43f4a97ed3fba552b0
Benjamin Wong <bycw@uw.edu>
2024-10-21 15:39:18 -0700

    test  new slam

--- scripts/anomaly_detector.py
+++ scripts/anomaly_detector.py
@@ -206,7 +206,7 @@ class Anomaly_Detector:
         # point_cov = node.local_map['cov'].copy()
         point_cov = cov
         # p, point_cov = self.random_down_sample(p, point_cov)
-        sigma_node = node.cov *0.1
+        sigma_node = node.cov 
         print(sigma_node)
         points = np.asarray(p.points)
         # sigma_node = np.eye(6) 
