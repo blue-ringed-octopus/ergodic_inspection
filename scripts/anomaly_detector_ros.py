ad35e6b63cb39729cd3e06167b6d363a26360eab
Benjamin Wong <bycw@uw.edu>
2024-10-31 14:26:43 -0700

    test

--- scripts/anomaly_detector_ros.py
+++ scripts/anomaly_detector_ros.py
@@ -61,8 +61,7 @@ class Anomaly_Detector_Wrapper:
         for region, candidate in candidates.items():
             msg = CandidatePoints()
             msg.region_id = region
-            if len(candidate)>0:
-                msg.points.layout = np.array(candidate.shape)
+            if len(candidate)>0:                
                 msg.points.data = candidate.reshape(-1)
             msgs.append(msg)
         return GetCandidatesResponse(msgs)
